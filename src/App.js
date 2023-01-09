81e627c9eb0a0fc375152981243063206173007a
JohnLu2004 <john.lu10212004@gmail.com>
2023-01-09 16:26:54 -0500

    Added the project section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import AboutMe from './AboutMe';
+import Project from './Projects'
 import Skills from './skills';
 import Contact from './Contact'
 
@@ -7,6 +8,15 @@ function App() {
   return (
     <div>
       <AboutMe />
+      <h1>Projects</h1>
+      <div class="row">
+        <Project projectName = "Project Name" projectTidBit = "About the Project" projectTools = "Anything made with it"/>
+        <Project projectName = "Website" projectTidBit = "Built to display attributes" projectTools = "React,HTML,CSS,JSX,GitHub"/>
+        <Project projectName = "Project Name" projectTidBit = "About the Project" projectTools = "Anything made with it"/>
+        <Project projectName = "Website" projectTidBit = "Built to display attributes" projectTools = "React,HTML,CSS,JSX,GitHub"/>
+        <Project projectName = "Project Name" projectTidBit = "About the Project" projectTools = "Anything made with it"/>
+        <Project projectName = "Website" projectTidBit = "Built to display attributes" projectTools = "React,HTML,CSS,JSX,GitHub"/>
+      </div>
       <Skills />
       <Contact />
     </div>
---
+++ src/Projects.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Projects.css';
+
+class Project extends React.Component {
+    render() {
+        return (
+            <div class="column">
+                <body>
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <h1>{this.props.projectName}</h1> 
+                                <p> {this.props.projectTidBit}</p>
+                            </div>
+                            <div class="flip-card-back">
+                                <h1>Made With</h1> 
+                                <p> {this.props.projectTools} </p>
+                            </div>
+                        </div>
+                    </div>
+                </body>
+            </div>
+        )
+    }
+}
+
+
+export default Project;
