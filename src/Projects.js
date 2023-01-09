b32c3013140589b7229589bb7e8293baf75635fd
MehBirb <john.lu10212004@gmail.com>
2023-01-08 21:12:47 -0500

    made more minor changes

--- src/Projects.js
+++ src/Projects.js
@@ -1,23 +1,27 @@
 import './Projects.css';
+
 const Projects = () => {
     return (
-        <body>
-        <h1>Card Flip with Text</h1>
-        <h3>Hover over the image below:</h3>
+        <div>
+            <body>
+                <h1>Card Flip with Text</h1>
+                <h3>Hover over the image below:</h3>
 
-        <div class="flip-card">
-        <div class="flip-card-inner">
-            <div class="flip-card-front">
-            <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;" />
-            </div>
-            <div class="flip-card-back">
-            <h1>John Doe</h1> 
-            <p>Architect & Engineer</p> 
-            <p>We love that guy</p>
-            </div>
-        </div>
-        </div>
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;" />
+                        </div>
+                        <div class="flip-card-back">
+                            <h1>John Doe</h1> 
+                            <p>Architect & Engineer</p> 
+                            <p>We love that guy</p>
+                        </div>
+                    </div>
+                </div>
 
-        </body>
-    )};
+            </body>
+        </div>
+    )
+};
  export default Projects;
