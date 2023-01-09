7ad849e392d45dec18fc2b7949f2609238546378
JohnLu2004 <john.lu10212004@gmail.com>
2023-01-09 00:27:39 -0500

    Can now mass produce by changing const arrow function into React component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Projects from './Projects';
 function App() {
   return (
     <div>
-      <Projects />
+      <Projects projectName = "Project Name" projectTidBit = "About the Project" projectTools = "Anything made with it"/>
+      <Projects projectName = "Website" projectTidBit = "Built to display attributes" projectTools = "React,HTML,CSS,JSX,GitHub"/>
     </div>
   );
 }
--- src/Projects.js
+++ src/Projects.js
@@ -1,27 +1,28 @@
+import React from 'react';
 import './Projects.css';
 
-const Projects = () => {
-    return (
-        <div>
-            <body>
-                <h1>Card Flip with Text</h1>
-                <h3>Hover over the image below:</h3>
-
-                <div class="flip-card">
-                    <div class="flip-card-inner">
-                        <div class="flip-card-front">
-                            <img src="pfp.jpg" alt="Avatar" width="300px" height="300px" />
-                        </div>
-                        <div class="flip-card-back">
-                            <h1>John Doe</h1> 
-                            <p>Architect & Engineer</p> 
-                            <p>We love that guy</p>
+class Project extends React.Component {
+    render() {
+        return (
+            <div>
+                <body>
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <h1>{this.props.projectName}</h1> 
+                                <p> {this.props.projectTidBit}</p>
+                            </div>
+                            <div class="flip-card-back">
+                                <h1>Made With</h1> 
+                                <p> {this.props.projectTools} </p>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </body>
+            </div>
+        )
+    }
+}
 
-            </body>
-        </div>
-    )
-};
- export default Projects;
+
+export default Project;
