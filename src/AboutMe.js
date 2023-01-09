bf136c2b0efddaeceb1168d13b1d7fc521e01463
Daniel-Arul <107771712+Daniel-Arul@users.noreply.github.com>
2023-01-08 19:42:42 -0500

    revised sizing and about me section

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -23,10 +23,8 @@ const AboutMe = () => {
     return ( 
       <div class = 'body'>
         <div class='text'>
-            <h1 class='name'><SplitText copy="Hi, My name is Bob Chen" role="heading" /></h1>
-            <h2 class = 'intro'><SplitText copy="I'm a Computer Science Major that loves" role="heading" /></h2>
-            <h2 class = 'intro'><SplitText copy="Hollow Knight, Eating chocos, and " role="heading" /></h2>
-            <h2 class = 'intro'><SplitText copy="having butterflies in ma tum tum :3" role="heading" /></h2>
+            <h1 class='name'><SplitText copy="Hi, My name is Bob Chen " role="heading" /></h1>
+            <h2 class = 'intro'><SplitText copy="I'm a Computer Science Major that loves Hollow Knight, Eating chocos, and having butterflies in ma tum tum :3" role="heading" /></h2>
           </div>
           <div class = 'inner'>
             <div id = 'image'>
