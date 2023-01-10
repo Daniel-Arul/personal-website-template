81cbaead8539282cf6520561d05538d7f41ae0b0
Gal Riger <riger.gal@gmail.com>
2023-01-10 16:10:11 -0500

    Added curves to skills

--- src/skills.js
+++ src/skills.js
@@ -3,7 +3,12 @@ import './defaults.css';
 
 const Skills = () => {
     return (
-        <section id="skills" class="skills">
+        <section id="skills" class="skills red">
+            <div class="wave">
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+                </svg>
+            </div>
             <h1 class = "Section-Title" id="techskills">Technical <span id="skillmark">Skills</span></h1>
             <div class="container">
                 <div class="skills-icons">
@@ -31,6 +36,11 @@ const Skills = () => {
                     <p class="skill-name">Fortnite</p>
                 </div>
                 {/* Keep adding divs to increase the line :) */}
+            </div>
+            <div class="wave-bottom">
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+                </svg>
             </div>
         </section>
     );
