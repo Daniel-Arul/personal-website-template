571e54b767a2941fc11d07e8e60171ad135e7d5f
JohnLu2004 <john.lu10212004@gmail.com>
2023-01-23 02:59:23 -0500

    made some progress but only able to have dot background in one section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import AboutMe from './AboutMe';
 import Projects from './Projects'
 import Skills from './skills';
 import Contact from './Contact'
+import Particless from './Particles'
+import './defaults.css';
+
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
 function App() {
   return (
@@ -12,6 +18,7 @@ function App() {
           <AboutMe />
         </div>
         <div id="Projects" class="Background-Blue">
+          <Particless/>
           <Projects />
         </div>
         <div id="Skills" class="Background-White">
@@ -20,12 +27,12 @@ function App() {
         <div id="Contact" class="Background-Blue">
           <Contact />
         </div>
-        <div class="navbar">
-        <a href="#About">About</a>
-        <a href="#Projects">Projects</a>
-        <a href="#Skills">Skills</a>
-        <a href="#Contact">Contact</a>
-      </div>
+          <div class="navbar">
+          <a href="#About">About</a>
+          <a href="#Projects">Projects</a>
+          <a href="#Skills">Skills</a>
+          <a href="#Contact">Contact</a>
+        </div>
       </div>
     </>
   );
