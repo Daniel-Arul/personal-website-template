8431da68987b397708ad31aaa39bfa531e99e271
JohnLu2004 <john.lu10212004@gmail.com>
2023-01-24 15:28:08 -0500

    Links for contacts take you to homepage

--- src/Contact.js
+++ src/Contact.js
@@ -10,23 +10,23 @@ const Contact = () => {
             </div>
             
         <div className="wrapper">
-                <a href="#" className="icon linkedin">
+                <a href="https://www.linkedin.com/company/tarteleria/" className="icon linkedin">
                     <div className="tooltip">linkedin</div>
                     <span><i className="fab fa-linkedin"></i></span>
                 </a>
-                <a href="#" className="icon twitter">
+                <a href="https://twitter.com/home" className="icon twitter">
                     <div className="tooltip">Twitter</div>
                     <span><i className="fab fa-twitter"></i></span>
                 </a>
-                <a href="#" className="icon instagram">
+                <a href="https://www.instagram.com/" className="icon instagram">
                     <div className="tooltip">Instagram</div>
                     <span><i className="fab fa-instagram"></i></span>
                 </a>
-                <a href="#" className="icon github">
+                <a href="https://github.com/" className="icon github">
                     <div className="tooltip">Github</div>
                     <span><i className="fab fa-github"></i></span>
                 </a>
-                <a href="#" className="icon youtube">
+                <a href="https://www.youtube.com/" className="icon youtube">
                     <div className="tooltip">Youtube</div>
                     <span><i className="fab fa-youtube"></i></span>
                 </a>
